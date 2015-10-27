d65ed735be195b992b2eb770df179a5d34a7f0f3
squal0 <root@localhost.localdomain>
2015-10-27 16:42:51 +0300

    finish user signup

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -10,11 +10,11 @@ class UserTest < ActiveSupport::TestCase
 	end
 
 	test "should be valid" do
-	 assert_not @user.valid?
+	 assert @user.valid?
 	end
 
 	test "name should be present" do
-	 @user.name = "        "
+	 @user.name = "     "
 	 assert_not @user.valid?
 	end
 
@@ -33,21 +33,21 @@ class UserTest < ActiveSupport::TestCase
 	 assert_not @user.valid?
 	end
 
-	test "email validation should accept valid addresses" do
-	 valid_addresses = %w[user@example.com USER@kill.COM A_US-ER@kill.bill.org first.last@kill.jp allie+ray@mayer.cn]
+	 test "email validation should accept valid addresses" do
+	 valid_addresses = %w[user@example.com USER@foo.COM A_US-ER@foo.bar.org first.last@foo.jp alice+bob@baz.cn]
 	 valid_addresses.each do |valid_address|
 	 @user.email = valid_address
-	 assert_not @user.valid?, "#{valid_address.inspect} should be valid"
-	end
-	end
+	 assert @user.valid?, "#{valid_address.inspect} should be valid"
+	 end
+	 end
 
-	test "email validation should reject invalid addresses" do
-	 invalid_addresses = %w[ user@example,com user_at_kill.org user.name@example. kill@bill_mayer.com kill@bill+mayer.com]
+	 test "email validation should reject invalid addresses" do
+	 invalid_addresses = %w[user@example,com user_at_foo.org user.name@example. foo@bar_baz.com foo@bar+baz.com]
 	 invalid_addresses.each do |invalid_address|
 	 @user.email = invalid_address
 	 assert_not @user.valid?, "#{invalid_address.inspect} should be invalid"
-	end
-	end
+	 end
+	 end
 
 	test "email addresses should be unique" do
 	 duplicate_user = @user.dup
